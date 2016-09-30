b5e2ad9e46639aae84f82a0dfe8d3c916783e786
Kael Pu <pwh0996@gmail.com>
2016-09-30 16:42:39 +0800

    fix bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,41 +2,33 @@ var flag = true;
 Page({
   data:{
     // text:"这是一个页面"
-    color: "window"  
+    color: "window"
   },
   click:function(){
-    console.log("点击了文字");
-      if(flag){
-        color = "window-red";
+    if(flag){
+        color = "window-new";
         flag = false;
-      }else{
+    }else{
         color = "window";
         flag = true;
-      }
-
-     console.log(color);
-      this.setData({
+    }
+    this.setData({
         color
-      });
+  });
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    console.log("Page OnLoad");
   },
   onReady:function(){
     // 页面渲染完成
-    console.log("Page onReady");
   },
   onShow:function(){
     // 页面显示
-    console.log("Page onShow");
   },
   onHide:function(){
     // 页面隐藏
-    console.log("Page onHide");
   },
   onUnload:function(){
     // 页面关闭
-    console.log("Page onUnload");
   }
 })
